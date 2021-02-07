f4dcc3340308ac06a303d8d5738e3e71cb0d2be8
Michael Sean De La Riva <michaeldlr17@gmail.com>
2021-02-06 16:29:22 -0800

    added necessary files for new page and new header, beginning to create pickup/delivery options section.  pushing temporary changes and continuing on

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
 import React, { Component } from 'react';
 import {BrowserRouter as Router, Route} from "react-router-dom";
+// =============== MAIN PAGE ================
 import MainHeader from "./includes/mainHeader.js";
 import JumboScreen from "./components/mainPage/jumboScreen.js";
 import Cuisine from "./components/mainPage/cuisineDisplay.js";
@@ -8,9 +9,12 @@ import Map from "./components/mainPage/mapSection.js";
 import Beer from "./components/mainPage/beerSection.js";
 import Reviews from "./components/mainPage/reviewSection.js";
 import TakeIt from "./components/mainPage/takeItHome.js";
+// =============== PICKUP/TAKEOUT ================
+import CartHeader from "./includes/cartHeader.js";
 import OrderJumbo from "./components/deliveryTakeout/orderJumbo.js";
+import Pickup from "./components/deliveryTakeout/pickupOptions.js";
+// =============== FOOTER ================
 import Footer from "./includes/footer.js";
-import CartHeader from "./includes/cartHeader.js"
 
 class App extends Component {
 	constructor() {
@@ -45,6 +49,7 @@ class App extends Component {
                 <Route exact path="/:order">
                     <CartHeader />
                     <OrderJumbo />
+                    <Pickup />
                 </Route>
             </div>
         </Router>
---
+++ src/components/deliveryTakeout/pickupOptions.js
@@ -0,0 +1,18 @@
+import React, {Component} from 'react';
+
+export default class Pickup extends Component{
+    constructor(){
+        super();
+        this.state={};
+    }
+
+    render(){
+        return(
+            <div className="pickupOptions">
+                <div className="marginContainer">
+                    <h1>Hi Ryan and Mariano!!!!!!!</h1>
+                </div>
+            </div>
+        );
+    }
+}
